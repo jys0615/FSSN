ea875b51bb173883e8ea0da983e3ed290db6a3c0
jys0615 <yoonsuh0615@khua.c.kr>
2025-12-06 22:16:40 +0900

    feat: add basic skeleton for bidirectional gRPC server and client

--- grpc/bidirectional_streaming/src/main/java/com/example/bidirectional/BidirectionalClient.java
+++ grpc/bidirectional_streaming/src/main/java/com/example/bidirectional/BidirectionalClient.java
@@ -1,3 +1,11 @@
+package com.example.bidirectional;
+
 public class BidirectionalClient {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Bidirectional Client running...");
+
+        // TODO: 채널 생성, stub 생성은 내일 진행
+    }
 }
---
+++ grpc/bidirectional_streaming/src/main/java/com/example/bidirectional/BidirectionalServer.java
@@ -0,0 +1,10 @@
+package com.example.bidirectional;
+
+public class BidirectionalServer {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bidirectional Server starting...");
+
+        // TODO: gRPC 서버 초기화는 나중에
+    }
+}
